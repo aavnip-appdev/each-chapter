962dbad9af268bb57449f74e15a6bf23231b0887
aavnip <aavnip@chicagobooth.edu>
2022-01-25 00:24:15 +0000

    Each Even Words WIP

--- each_even_word.rb
+++ each_even_word.rb
@@ -1,13 +1,13 @@
 # Write a program that:
 #   Asks the user to enter a list of words separated by spaces.
 #   The program should only print the words that are have an even number of characters
-# 
+#
 
 p "Enter a list of words separated by spaces:"
 input_words = gets.chomp.split
 
 input_words.each do |even_word|
-  if even_word.length % 2 == 0 
+  if even_word.length % 2 == 0
     p even_word
   end
 end
--- each_letter_count.rb
+++ each_letter_count.rb
@@ -4,3 +4,9 @@
 #  The program should print each letter in the word the number of times it appears in the word.
 
 p "Enter a word:"
+input_letters = gets.chomp.split("")
+p input_letters
+
+input_letters.each do |letter|
+  p letter
+end 
